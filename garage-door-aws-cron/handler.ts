54c3ebda8de14bc60747489d989f4a522e519cbe
Davi Mello <davimello1994@gmail.com>
2023-03-07 18:14:11 -0500

    User story: re-signifing handler.ts

--- garage-door-aws-cron/handler.ts
+++ garage-door-aws-cron/handler.ts
@@ -1,6 +1,3 @@
 import { getBodyMessage, sendMessage } from './message-service';
 
-export const getGarageUpdates = getBodyMessage().then((update) => {
-  const time = new Date();
-  console.log(`Your message ${update} ran at ${time}`);
-});
+export const getGarageUpdates = getBodyMessage();
