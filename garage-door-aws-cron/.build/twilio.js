490fa382e9aadd7c0dcc1ca27d4c19c297ee37bc
Davi Mello <davimello1994@gmail.com>
2023-03-09 16:02:45 -0500

    Tech debt: ignoring build folder

--- garage-door-aws-cron/.build/twilio.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.client = void 0;
-var accountSid = process.env.TWILIO_ACCOUNT_SID;
-var authToken = process.env.TWILIO_AUTH_TOKEN;
-var twilio = require("twilio");
-exports.client = new twilio(accountSid, authToken);
-//# sourceMappingURL=twilio.js.map
