6e599b7c5a6d502b11b3ff23b3d5ee2bf984d37c
Davi Mello <davimello1994@gmail.com>
2023-03-11 14:11:51 -0500

    Code: re-writting service methods

--- handler.ts
+++ handler.ts
@@ -4,7 +4,7 @@ exports.getGarageUpdates = async (event: any) => {
   const message = await sendMessage();
   const response = {
     statusCode: message?.statusCode,
-    body: JSON.stringify(message?.message)
+    body: JSON.stringify(message?.body)
   }
 
   return response;
--- message-service.ts
+++ message-service.ts
@@ -5,10 +5,11 @@ import { myQApi, myQDeviceInterface } from "@hjdhjd/myq";
 import { client } from "./twilio";
 
 type State = myQDeviceInterface["state"];
+type DoorState = State["door_state"];
 
 interface MessageResponse {
   statusCode: number;
-  message: string;
+  body: string;
 }
 
 const username = process.env.MY_Q_EMAIL as string;
@@ -17,8 +18,7 @@ const password = process.env.MY_Q_PASSWORD as string;
 const from = process.env.TWILIO_FROM_NUMBER;
 const to = process.env.TWILIO_TO_NUMBER;
 
-const fetchGarageDoorState: Promise<State | undefined> = new Promise(
-  async (resolve, reject) => {
+const fetchGarageDoorState = async (): Promise<State> => {
     try {
       const myQ = new myQApi(username, password);
       await myQ.refreshDevices();
@@ -27,14 +27,13 @@ const fetchGarageDoorState: Promise<State | undefined> = new Promise(
       const garageDoor: Readonly<myQDeviceInterface> | undefined =
         devicesInfo.find((v) => v.device_platform === "myq");
 
-      resolve(garageDoor?.state as State);
+      return garageDoor?.state as State;
       // Commenting this out for now:
       // const openGarage = myQ.execute(garageDoor as Readonly<myQDeviceInterface>, 'close');
     } catch (error) {
-      reject(error);
+      return error;
     }
-  }
-);
+  };
 
 const getDate = (date: Date): string => {
   const messageDate = new Date(date);
@@ -44,7 +43,13 @@ const getDate = (date: Date): string => {
   const diffInDays = timeDiff / (1000 * 60 * 60 * 24);
   if (diffInDays < 1) {
     // Format time in AM/PM format
-    const hours = messageDate.getHours();
+    const hours = Number(
+      messageDate.toLocaleString("en-US", {
+        timeZone: "America/New_York",
+        hour: "numeric",
+        hour12: false,
+      })
+    );
     const minutes = messageDate.getMinutes().toString().padStart(2, "0");
     const amPm = hours >= 12 ? "pm" : "am";
     const formattedHours = hours % 12 || 12;
@@ -58,33 +63,42 @@ const getDate = (date: Date): string => {
   }
 };
 
-export const getBodyMessage = (): Promise<MessageResponse> => {
-  return fetchGarageDoorState
-    .then((garageDoorState) => {
-      const lastUpdate = new Date(garageDoorState?.last_update as string);
-      const timeAgo = getDate(lastUpdate);
+const createBodyMessage = (garageDoorState: State): MessageResponse => {
+  const lastUpdate = new Date(garageDoorState?.last_update as string);
+  const timeAgo = getDate(lastUpdate);
 
-      return {
-        statusCode: 200,
-        message: `\nCurrent garage state: ${garageDoorState?.door_state}\nLast time updated: ${timeAgo}`,
-      };
-    })
-    .catch((error: Error) => {
+
+  if (garageDoorState && garageDoorState?.door_state.toLowerCase() == "closed") {
+    return { statusCode: 200, body: '' };
+  }
+
+  return { statusCode: 200, body: `\nCurrent garage state: ${garageDoorState?.door_state}\nLast time updated: ${timeAgo}` };
+}
+
+export const getBodyMessage = async (): Promise<MessageResponse> => {
+  try {
+    const response = await fetchGarageDoorState();
+    const message = createBodyMessage(response);
+
+    return message;
+  }
+  catch(error) {
       return {
         statusCode: 500,
-        message: `\nThe following error has occurred: ${error.message}`,
+        body: `\nThe following error has occurred: ${error.message}`,
       };
-    });
+    };
 };
 
 export const sendMessage = async (): Promise<MessageResponse> => {
   const messageResponse = await getBodyMessage();
 
-  await client.messages.create({
-    body: messageResponse?.message,
-    from: from,
-    to: to,
-  });
-
+  if (messageResponse?.body.length > 0) {
+    await client.messages.create({
+      body: messageResponse?.body,
+      from: from,
+      to: to,
+    });
+  }
   return messageResponse;
 };
